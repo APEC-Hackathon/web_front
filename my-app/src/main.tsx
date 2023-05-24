2457adacd8abe10c0b85cbd254b8c372d7a231f0
arnav-ag <arnav.ctf@gmail.com>
2023-05-24 20:15:02 +0200

    CSS issues + file-based router

--- my-app/src/main.tsx
+++ my-app/src/main.tsx
@@ -1,10 +1,8 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import "bulma/css/bulma.min.css";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+import { createRoot } from "react-dom/client";
+import { Routes } from "@generouted/react-router";
+
+const container = document.getElementById("app")!;
+createRoot(container).render(<Routes />);
