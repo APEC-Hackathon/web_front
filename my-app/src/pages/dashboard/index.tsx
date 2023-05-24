25a8de2a6379248dadb2811bccbb2bb8f6764da5
arnav-ag <arnav.ctf@gmail.com>
2023-05-24 19:46:07 +0200

    Use typescript

---
+++ my-app/src/pages/dashboard/index.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Dashboard = () => {
+    return (
+        <div className="main">
+            <div className="buttons">
+                <button class="button is-primary">Primary</button>
+                <button class="button is-link">Link</button>
+                <button class="button is-info">Info</button>
+                <button class="button is-success">Success</button>
+                <button class="button is-warning">Warning</button>
+                <button class="button is-danger">Danger</button>
+                <button class="button is-black">Black</button>
+                <button class="button is-white">White</button>
+                <button class="button is-dark">Dark</button>
+                <button class="button is-light">Light</button>
+            </div>
+        </div>
+    );
+};
+
+export default Dashboard;
