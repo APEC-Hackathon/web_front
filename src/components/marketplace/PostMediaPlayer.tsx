063c90e542c675cafbee2a7af37288cffdcb3013
Eugene <eugene.chiay@gmail.com>
2023-05-28 22:48:48 +0200

    Post media player

--- src/components/marketplace/PostMediaPlayer.tsx
+++ src/components/marketplace/PostMediaPlayer.tsx
@@ -1,69 +1,114 @@
-import React, { useEffect, useState } from "react";
-import { Carousel } from "react-responsive-carousel";
+import './MediaPlayer.css'
+import React, {useEffect, useState, useRef} from "react";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
+import {Box, Stack} from "@mui/material";
 
 const PostMediaPlayer = () => {
-    const [images, setImages] = useState<string[]>([]);
-    const [isLoading, setIsLoading] = useState(true);
+    const [nav1, setNav1] = useState<Slider | null>(null);
+    const [nav2, setNav2] = useState<Slider | null>(null);
+    const slider1 = useRef<Slider | null>(null);
+    const slider2 = useRef<Slider | null>(null);
 
     useEffect(() => {
-        const fetchImages = async () => {
-            try {
-                const fetchedImages: string[] = [];
+        if (slider1.current && slider2.current) {
+            setNav1(slider1.current);
+            setNav2(slider2.current);
+        }
+    }, []);
 
-                for (let i = 0; i < 5; i++) {
-                    const response = await fetch(
-                        "https://api.api-ninjas.com/v1/randomimage?category=food",
-                        {
-                            headers: {
-                                "X-Api-Key": "OBtZqShuHuJs0HC3LDfN4g==U3ysgJgIb2LHHCx4",
-                                Accept: "image/jpg",
-                            },
-                        }
-                    );
-
-                    const blob = await response.blob();
-                    const reader = new FileReader();
-
-                    reader.onloadend = () => {
-                        if (typeof reader.result === "string") {
-                            fetchedImages.push(reader.result);
-
-                            // Check if all images have been fetched
-                            if (fetchedImages.length === 5) {
-                                setImages(fetchedImages);
-                                setIsLoading(false);
-                            }
-                        }
-                    };
-
-                    reader.readAsDataURL(blob);
-                }
-            } catch (error) {
-                console.error("Error fetching images:", error);
-            }
-        };
-
-        fetchImages();
-    }, []); // Empty dependency array ensures the effect runs only once
-
-    const getRandomImages = (count: number): string[] => {
-        const shuffledImages = [...images].sort(() => 0.5 - Math.random());
-        return shuffledImages.slice(0, count);
+    const settingsMain = {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: true,
+        dots: true,
+        fade: true,
+        asNavFor: ".slider-nav",
     };
 
-    if (isLoading) {
-        return <div>Loading...</div>;
-    }
+    const settingsThumbs = {
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        asNavFor: ".slider-for",
+        centerMode: true,
+        swipeToSlide: true,
+        focusOnSelect: true,
+        infinite: true,
+    };
+
+    const slidesData = [
+        {
+            id: 1,
+            title: "repellendus id ullam",
+            label: "Dolorem officiis temporibus.",
+        },
+        {
+            id: 2,
+            title: "excepturi consequatur est",
+            label: "Officia non provident dolor esse et neque.",
+        },
+        {
+            id: 3,
+            title: "eius doloribus blanditiis",
+            label: "Ut recusandae vel vitae molestiae id soluta.",
+        },
+        {
+            id: 4,
+            title: "nihil voluptates delectus",
+            label: "Qui vel consequatur recusandae illo repellendus.",
+        },
+        {
+            id: 5,
+            title: "nemo dolorem necessitatibus",
+            label: "Placeat odit velit itaque voluptatem.",
+        },
+    ];
+
 
     return (
-        <Carousel axis="horizontal">
-            {getRandomImages(5).map((image, index) => (
-                <div key={index}>
-                    <img src={image} alt={`${index}`} />
+        <Stack
+            direction="row"
+        >
+            <Box sx={{ width: "85%", pr: "10px"}}>
+                <Slider
+                    {...settingsMain}
+                    asNavFor={nav2!}
+                    ref={(slider) => (slider1.current = slider)}
+                >
+                    {slidesData.map((slide) => (
+                        <div className="slick-slide" key={slide.id}>
+                            <img
+                                className="slick-slide-image"
+                                src={`https://picsum.photos/1920/1080?img=${slide.id}`}
+                                alt={slide.title}
+                            />
+                        </div>
+                    ))}
+                </Slider>
+            </Box>
+            <Box sx={{ width: "15%", height: "100%" }}>
+                <div className="thumbnail-slider-wrap">
+                <Slider
+                    {...settingsThumbs}
+                    asNavFor={nav1!}
+                    ref={(slider) => (slider2.current = slider)}
+                    vertical={true}
+                >
+                    {slidesData.map((slide) => (
+                        <div className="slick-slide" key={slide.id}>
+                            <img
+                                className="slick-slide-image"
+                                src={`https://picsum.photos/1920/1080?img=${slide.id}`}
+                                alt={slide.title}
+                            />
+                        </div>
+                    ))}
+                </Slider>
                 </div>
-            ))}
-        </Carousel>
+            </Box>
+        </Stack>
     );
 };
 
