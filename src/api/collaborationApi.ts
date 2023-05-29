1d1e58c2fb5ab899454a84a8e7197baec84cf432
nvbinh15 <binh.pdc01@gmail.com>
2023-05-29 15:57:54 +0200

    Add collaboration and problem API middleware

--- src/api/collaborationApi.ts
+++ src/api/collaborationApi.ts
@@ -10,7 +10,33 @@ const collaborationApi = {
         setHeaders(); setBaseURL(); setCredentials()
         return CollaborationService.readMyCollaborationsApiV1CollaborationGet(skip, limit)
     },
-    createCollaborationBid: (collaborationId: number, bid: string) => {
+    getAllCollaborations: (skip?: number, limit: number = 100) => {
+        setHeaders(); setBaseURL(); setCredentials()
+        return CollaborationService.readAllCollaborationsApiV1CollaborationFeedGet(skip, limit)
+    },
+    createCollaboration: (title: string, description: string, source_id: number, image_url: string) => {
+        setHeaders(); setBaseURL(); setCredentials()
+        return CollaborationService.createCollaborationApiV1CollaborationPost({
+            title: title,
+            description: description,
+            source_id: source_id,
+            image_url: image_url
+        })
+    },
+    updateCollaboration: (collaborationId: number, title: string, description: string, source_id: number, image_url: string) => {
+        setHeaders(); setBaseURL(); setCredentials()
+        return CollaborationService.updateMyCollaborationApiV1CollaborationCollaborationIdPut(collaborationId, {
+            title: title,
+            description: description,
+            source_id: source_id,
+            image_url: image_url
+        })
+    },
+    deleteCollaboration: (collaborationId: number) => {
+        setHeaders(); setBaseURL(); setCredentials()
+        return CollaborationService.deleteCollaborationApiV1CollaborationCollaborationIdDelete(collaborationId)
+    },
+    createCollaborationRequest: (collaborationId: number, bid: string) => {
         setHeaders(); setBaseURL(); setCredentials()
         return CollaborationService.createCollaborationRequestApiV1CollaborationRequestPost(
             {
@@ -18,6 +44,39 @@ const collaborationApi = {
                 description: bid
             }
         )
+    },
+    getCollaborationRequestById: (collaborationRequestId: number) => {
+        setHeaders(); setBaseURL(); setCredentials()
+        return CollaborationService.readCollaborationRequestByIdApiV1CollaborationRequestCollaborationRequestIdGet(collaborationRequestId)
+    },
+    getMyCollaborationRequests: (skip?: number, limit: number = 100) => {
+        setHeaders(); setBaseURL(); setCredentials()
+        return CollaborationService.readMyCollaborationRequestsApiV1CollaborationMyRequestsGet(skip, limit)
+    },
+    getRequestsForCollaboration: (collaborationId: number) => {
+        setHeaders(); setBaseURL(); setCredentials()
+        return CollaborationService.readCollaborationRequestsApiV1CollaborationRequestsCollaborationIdGet(collaborationId)
+    },
+    updateCollaborationRequest: (collaborationRequestId: number, description: string) => {
+        setHeaders(); setBaseURL(); setCredentials()
+        return CollaborationService.updateMyCollaborationRequestApiV1CollaborationRequestCollaborationRequestIdPut(
+            collaborationRequestId, 
+            {
+                description: description
+            }
+        )
+    },
+    deleteCollaborationRequest: (collaborationRequestId: number) => {
+        setHeaders(); setBaseURL(); setCredentials()
+        return CollaborationService.deleteCollaborationRequestApiV1CollaborationRequestCollaborationRequestIdDelete(collaborationRequestId)
+    },
+    acceptCollaborationRequest: (collaborationRequestId: number) => {
+        setHeaders(); setBaseURL(); setCredentials()
+        return CollaborationService.acceptCollaborationRequestApiV1CollaborationRequestCollaborationRequestIdAcceptPut(collaborationRequestId)
+    },
+    rejectCollaborationRequest: (collaborationRequestId: number) => {
+        setHeaders(); setBaseURL(); setCredentials()
+        return CollaborationService.rejectCollaborationRequestApiV1CollaborationRequestCollaborationRequestIdRejectPut(collaborationRequestId)
     }
 }
 
--- src/api/problemApi.ts
+++ src/api/problemApi.ts
@@ -4,21 +4,74 @@ import { setHeaders, setBaseURL, setCredentials } from "./utils";
 
 
 const problemApi = {
-  getProblemById: (problemId: number) => {
-    setHeaders(); setBaseURL(); setCredentials()
-    return ProblemService.readProblemByIdApiV1ProblemProblemIdGet(problemId);
-  },
-  getMyProblems: (skip?: number, limit: number = 100) => {
-    setHeaders(); setBaseURL(); setCredentials()
-    return ProblemService.readMyProblemsApiV1ProblemGet(skip, limit);
-  },
-  createProblemBid: (problemId: number, bid: string) => {
-    setHeaders(); setBaseURL(); setCredentials()
-    return ProblemService.createProblemBidApiV1ProblemBidPost({
-      problem_id: problemId,
-      description: bid
-    });
-  }
+    getProblemById: (problemId: number) => {
+        setHeaders(); setBaseURL(); setCredentials()
+        return ProblemService.readProblemByIdApiV1ProblemProblemIdGet(problemId);
+    },
+    getMyProblems: (skip?: number, limit: number = 100) => {
+        setHeaders(); setBaseURL(); setCredentials()
+        return ProblemService.readMyProblemsApiV1ProblemGet(skip, limit);
+    },
+    getAllProblems: (skip?: number, limit: number = 100) => {
+        setHeaders(); setBaseURL(); setCredentials()
+        return ProblemService.readAllProblemsApiV1ProblemFeedGet(skip, limit);
+    },
+    createProblem: (title: string, description: string, bid_deadline: string, image_url: string) => {
+        setHeaders(); setBaseURL(); setCredentials()
+        return ProblemService.createProblemApiV1ProblemPost({
+            title: title,
+            description: description,
+            bid_deadline: bid_deadline,
+            image_url: image_url
+        });
+    },
+    updateProblem: (problemId: number, title: string, description: string, bid_deadline: string, image_url: string, bid_winner_id: number) => {
+        setHeaders(); setBaseURL(); setCredentials()
+        return ProblemService.updateMyProblemApiV1ProblemProblemIdPut(problemId, {
+            title: title,
+            description: description,
+            bid_deadline: bid_deadline,
+            image_url: image_url, 
+            bid_winner_id: bid_winner_id
+        });
+    },
+    deleteProblem: (problemId: number) => {
+        setHeaders(); setBaseURL(); setCredentials()
+        return ProblemService.deleteProblemApiV1ProblemProblemIdDelete(problemId);
+    },
+    createProblemBid: (problemId: number, bid: string) => {
+        setHeaders(); setBaseURL(); setCredentials()
+        return ProblemService.createProblemBidApiV1ProblemBidPost({
+            problem_id: problemId,
+            description: bid
+        });
+    },
+    getProblemBidById: (problemBidId: number) => {
+        setHeaders(); setBaseURL(); setCredentials()
+        return ProblemService.readProblemBidByIdApiV1ProblemBidProblemBidIdGet(problemBidId);
+    },
+    getMyProblemBids: (skip?: number, limit: number = 100) => {
+        setHeaders(); setBaseURL(); setCredentials()
+        return ProblemService.readMyProblemBidsApiV1ProblemMyBidsGet(skip, limit);
+    },
+    getBidsForProblem: (problemId: number) => {
+        setHeaders(); setBaseURL(); setCredentials()
+        return ProblemService.readAllBidsForAProblemApiV1ProblemBidsProblemIdGet(problemId);
+    },
+    updateProblemBid: (problemBidId: number, description: string) => {
+        setHeaders(); setBaseURL(); setCredentials()
+        return ProblemService.updateMyProblemBidApiV1ProblemBidProblemBidIdPut(problemBidId, {
+            description: description
+        });
+    },
+    deleteProblemBid: (problemBidId: number) => {
+        setHeaders(); setBaseURL(); setCredentials()
+        return ProblemService.deleteProblemBidApiV1ProblemBidProblemBidIdDelete(problemBidId);
+    },
+    chooseProblemBidWinner: (problemBidId: number, problemBidWinnerId: number) => {
+        setHeaders(); setBaseURL(); setCredentials()
+        return ProblemService.chooseWinnerForProblemApiV1ProblemProblemIdAnnouncePost(problemBidId, problemBidWinnerId)
+    }
 };
 
 export default problemApi;
