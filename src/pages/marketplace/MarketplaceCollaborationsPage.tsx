f0b99e315a8c3fb67d72c2f7400c9b001042a7c6
nvbinh15 <binh.pdc01@gmail.com>
2023-05-30 03:35:19 +0200

    Fix display of collaboration and problem card

--- src/pages/marketplace/MarketplaceCollaborationsPage.tsx
+++ src/pages/marketplace/MarketplaceCollaborationsPage.tsx
@@ -1,62 +1,74 @@
 import React from "react";
-import {Grid, Box, Typography, Stack} from "@mui/material";
-import InfiniteScroll from 'react-infinite-scroller';
-import {Collaboration} from "../../client";
+import { Grid, Box, Typography, Stack } from "@mui/material";
+import InfiniteScroll from "react-infinite-scroller";
+import { Collaboration } from "../../client";
 import collaborationApi from "../../api/collaborationApi";
 import CollaborationCard from "../../components/marketplace/CollaborationCard";
 import MarketPlaceSearchBar from "../../components/marketplace/MarketPlaceSearchBar";
 import CreateCollaborationButton from "../../components/marketplace/CreateCollaborationButton";
 
 const MarketplaceCollaborationsPage = () => {
-    const [collaborations, setUsers] = React.useState<Collaboration[]>([]);
-    const [reloadCount, setReloadCount] = React.useState<number>(0);
-    const [hasMore, setHasMore] = React.useState<boolean>(true);
-    const fetchCompanies = async () => {
-        try {
-            if (reloadCount > 10) {
-                setHasMore(false);
-                return;
-            }
-            // To change to retrieve a limit of 10
-            const moreCollaborations = await collaborationApi.getAllCollaborations()
-            setUsers([...collaborations, ...moreCollaborations]);
-            setReloadCount((reloadCount) => reloadCount + 1);
-        } catch (error) {
-            console.error("Error fetching user problems:", error);
+  const [collaborations, setCollaborations] = React.useState<Collaboration[]>([]);
+  const [reloadCount, setReloadCount] = React.useState<number>(0);
+  const [hasMore, setHasMore] = React.useState<boolean>(true);
+
+  const fetchCollaborations = async () => {
+    try {
+      if (reloadCount > 10) {
+        setHasMore(false);
+        return;
+      }
+      // Change to retrieve a limit of 10
+      const moreCollaborations = await collaborationApi.getAllCollaborations();
+      setCollaborations([...collaborations, ...moreCollaborations]);
+      setReloadCount((reloadCount) => reloadCount + 1);
+    } catch (error) {
+      console.error("Error fetching collaborations:", error);
+    }
+  };
+
+  return (
+    <Box>
+      <Stack direction="row" alignItems="center">
+        <MarketPlaceSearchBar type={"collaboration"} />
+        <CreateCollaborationButton />
+      </Stack>
+      <InfiniteScroll
+        pageStart={0}
+        loadMore={fetchCollaborations}
+        hasMore={hasMore}
+        loader={
+          <div className="loader" key="loader">
+            Loading ...
+          </div>
         }
-    };
-
-    return (
-        <Box>
-            <Stack direction="row" alignItems="center">
-                <MarketPlaceSearchBar type={"collaboration"}/>
-                <CreateCollaborationButton/>
-            </Stack>
-            <InfiniteScroll
-                pageStart={0}
-                loadMore={fetchCompanies}
-                hasMore={hasMore}
-                loader={
-                    <div className="loader" key="loader">
-                        Loading ...
-                    </div>
-                }
-            >
-                <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
-                    {collaborations!.map((collaboration) => (<CollaborationCard collaborationId={collaboration.id!}/>))}
-                </Grid>
-                {!hasMore && (
-                    <Box sx={{display: 'flex', justifyContent: 'center', alignItems: 'center', height: '10vh'}}>
-                        <Typography variant="h6" component="div" gutterBottom>
-                            That's all folks!
-                        </Typography>
-                    </Box>
-                )
-                }
-            </InfiniteScroll>
-
-        </Box>
-    )
+      >
+        <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
+          {collaborations.map((collaboration) => (
+            <Grid item key={collaboration.id} xs={12} sm={6} md={4} lg={3}>
+              <Box sx={{ height: "100%" }}>
+                <CollaborationCard collaboration={collaboration} />
+              </Box>
+            </Grid>
+          ))}
+        </Grid>
+        {!hasMore && (
+          <Box
+            sx={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              height: "10vh",
+            }}
+          >
+            <Typography variant="h6" component="div" gutterBottom>
+              That's all folks!
+            </Typography>
+          </Box>
+        )}
+      </InfiniteScroll>
+    </Box>
+  );
 };
 
 export default MarketplaceCollaborationsPage;
--- src/pages/marketplace/MarketplaceProblemsPage.tsx
+++ src/pages/marketplace/MarketplaceProblemsPage.tsx
@@ -1,62 +1,74 @@
 import React from "react";
-import {Grid, Box, Typography, Stack} from "@mui/material";
-import InfiniteScroll from 'react-infinite-scroller';
-import {Problem} from "../../client";
+import { Grid, Box, Typography, Stack } from "@mui/material";
+import InfiniteScroll from "react-infinite-scroller";
+import { Problem } from "../../client";
 import problemApi from "../../api/problemApi";
 import ProblemCard from "../../components/marketplace/ProblemCard";
 import MarketPlaceSearchBar from "../../components/marketplace/MarketPlaceSearchBar";
 import CreateProblemButton from "../../components/marketplace/CreateProblemButton";
 
 const MarketplaceProblemsPage = () => {
-    const [problems, setUsers] = React.useState<Problem[]>([]);
-    const [reloadCount, setReloadCount] = React.useState<number>(0);
-    const [hasMore, setHasMore] = React.useState<boolean>(true);
-    const fetchCompanies = async () => {
-        try {
-            if (reloadCount > 10) {
-                setHasMore(false);
-                return;
-            }
-            // To change to retrieve a limit of 10
-            const moreProblems = await problemApi.getAllProblems()
-            setUsers([...problems, ...moreProblems]);
-            setReloadCount((reloadCount) => reloadCount + 1);
-        } catch (error) {
-            console.error("Error fetching user problems:", error);
+  const [problems, setProblems] = React.useState<Problem[]>([]);
+  const [reloadCount, setReloadCount] = React.useState<number>(0);
+  const [hasMore, setHasMore] = React.useState<boolean>(true);
+
+  const fetchProblems = async () => {
+    try {
+      if (reloadCount > 10) {
+        setHasMore(false);
+        return;
+      }
+      // Change to retrieve a limit of 10
+      const moreProblems = await problemApi.getAllProblems();
+      setProblems([...problems, ...moreProblems]);
+      setReloadCount((reloadCount) => reloadCount + 1);
+    } catch (error) {
+      console.error("Error fetching problems:", error);
+    }
+  };
+
+  return (
+    <Box>
+      <Stack direction="row" alignItems="center">
+        <MarketPlaceSearchBar type={"problem"} />
+        <CreateProblemButton />
+      </Stack>
+      <InfiniteScroll
+        pageStart={0}
+        loadMore={fetchProblems}
+        hasMore={hasMore}
+        loader={
+          <div className="loader" key="loader">
+            Loading ...
+          </div>
         }
-    };
-
-    return (
-        <Box>
-            <Stack direction="row" alignItems="center">
-                <MarketPlaceSearchBar type={"problem"}/>
-                <CreateProblemButton/>
-            </Stack>
-            <InfiniteScroll
-                pageStart={0}
-                loadMore={fetchCompanies}
-                hasMore={hasMore}
-                loader={
-                    <div className="loader" key="loader">
-                        Loading ...
-                    </div>
-                }
-            >
-                <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
-                    {problems!.map((problem) => (<ProblemCard problemId={problem.id!}/>))}
-                </Grid>
-                {!hasMore && (
-                    <Box sx={{display: 'flex', justifyContent: 'center', alignItems: 'center', height: '10vh'}}>
-                        <Typography variant="h6" component="div" gutterBottom>
-                            That's all folks!
-                        </Typography>
-                    </Box>
-                )
-                }
-            </InfiniteScroll>
-
-        </Box>
-    )
+      >
+        <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
+          {problems.map((problem) => (
+            <Grid item key={problem.id} xs={12} sm={6} md={4} lg={3}>
+              <Box sx={{ height: "100%" }}>
+                <ProblemCard problem={problem} />
+              </Box>
+            </Grid>
+          ))}
+        </Grid>
+        {!hasMore && (
+          <Box
+            sx={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              height: "10vh",
+            }}
+          >
+            <Typography variant="h6" component="div" gutterBottom>
+              That's all folks!
+            </Typography>
+          </Box>
+        )}
+      </InfiniteScroll>
+    </Box>
+  );
 };
 
 export default MarketplaceProblemsPage;
