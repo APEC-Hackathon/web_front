75d3b2d557e6efc4a8c14d81b5a6c896b611126a
Eugene <eugene.chiay@gmail.com>
2023-05-30 03:45:54 +0200

    change sidebar marketplace and collaborations

--- src/configs/colorConfigs.ts
+++ src/configs/colorConfigs.ts
@@ -2,14 +2,14 @@ import { colors } from "@mui/material";
 
 const colorConfigs = {
   sidebar: {
-    bg: "#2E186A",
+    bg: "#183c64",
     color: "#eeeeee",
-    hoverBg: "#eb8549",
-    activeBg: "#fe7625"
+    hoverBg: "#18641A",
+    activeBg: "#644018"
   },
   topbar: {
     bg: "#fff",
-    color: "#000"
+    color: "#183c64"
   },
   mainBg: colors.grey["100"]
 };
